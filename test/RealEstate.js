722c484b41e49c438711b7c677019158d514a056
Vincent <v3rdugo@yahoo.com>
2022-08-14 14:56:08 -0700

    refactor, add inspector and lender

--- test/RealEstate.js
+++ test/RealEstate.js
@@ -1,11 +1,20 @@
 const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
+const tokens = (n) =>
+{
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
 describe('RealEstate', () =>
 {
     let realEstate, escrow
-    let accounts, deployer, seller
+    let accounts, deployer, seller, buyer, inspector, lender
     const NFT_ID = 1
+    const PURCHASE_PRICE = ether(100)
+    const ESCROW_AMOUNT = ether(20)
 
     beforeEach(async () =>
     {
@@ -13,12 +22,23 @@ describe('RealEstate', () =>
         deployer = accounts[0]
         seller = deployer
         buyer = accounts[1]
+        inspector = accounts[2]
+        lender = accounts[3]
 
         const RealEstate = await ethers.getContractFactory('RealEstate')
         const Escrow = await ethers.getContractFactory('Escrow')
 
         realEstate = await RealEstate.deploy()
-        escrow = await Escrow.deploy(realEstate.address, NFT_ID, seller.address, buyer.address)
+        escrow = await Escrow.deploy(
+            realEstate.address, 
+            NFT_ID, 
+            PURCHASE_PRICE,
+            ESCROW_AMOUNT,
+            seller.address, 
+            buyer.address,
+            inspector.address,
+            lender.address
+            )
 
         // seller approves NFT
         transaction = await realEstate.connect(seller).approve(escrow.address, NFT_ID)
@@ -35,10 +55,15 @@ describe('RealEstate', () =>
 
     describe('Selling real estate', async () =>
     {
+        let balance, transaction
+
         it('executes a successful transaction', async () =>
         {
             // expects the seller to be the NFT owner before the sale
             expect(await realEstate.ownerOf(NFT_ID)).to.equal(seller.address)
+
+            // buyer deposits earnest
+            transaction = await escrow.connect(buyer).depositEarnest({value: ESCROW_AMOUNT})
 
             // finalize the sale
             transaction = await escrow.connect(buyer).finalizeSale()
