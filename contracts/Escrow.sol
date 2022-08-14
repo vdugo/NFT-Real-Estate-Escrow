d604bc03e01c081783e1ec7cd309372b3195879a
Vincent <v3rdugo@yahoo.com>
2022-08-14 15:08:37 -0700

    depositing earnest (down payment) working

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -7,15 +7,43 @@ contract Escrow
 {
     address public nftAddress;
     uint256 public nftId;
+    uint256 public purchasePrice;
+    uint256 public escrowAmount;
     address payable public seller;
     address payable public buyer;
+    address public inspector;
+    address public lender;
 
-    constructor(address _nftAddress, uint256 _nftId, address payable _seller, address payable _buyer)
+    constructor(
+        address _nftAddress, 
+        uint256 _nftId, 
+        uint256 _purchasePrice,
+        uint256 _escrowAmount, 
+        address payable _seller, 
+        address payable _buyer,
+        address _inspector,
+        address _lender
+        )
     {
         nftAddress = _nftAddress;
         nftId = _nftId;
+        purchasePrice = _purchasePrice;
+        escrowAmount = _escrowAmount;
         seller = _seller;
         buyer = _buyer;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+    function depositEarnest() public payable
+    {
+        require(msg.value >= escrowAmount, "earnest not enough");
+        require(msg.sender == buyer, "must be buyer");
+    }
+
+    function getBalance() public view returns(uint256)
+    {
+        return address(this).balance;
     }
 
     /**
