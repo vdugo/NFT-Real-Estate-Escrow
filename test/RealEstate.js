38d1b018153661f7801b3160dd2acb9238a022d6
Vincent <v3rdugo@yahoo.com>
2022-08-13 22:51:34 -0700

    add a test file, check to see if the RealEstate contract sends an NFT to deployer

---
+++ test/RealEstate.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+describe('RealEstate', () =>
+{
+    let realEstate, escrow
+    let accounts, deployer, seller
+    const NFT_ID = 1
+
+    beforeEach(async () =>
+    {
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
+        seller = deployer
+
+        const RealEstate = await ethers.getContractFactory('RealEstate')
+        const Escrow = await ethers.getContractFactory('Escrow')
+
+        realEstate = await RealEstate.deploy()
+        escrow = await Escrow.deploy()
+    })
+
+    describe('Deployment', async () =>
+    {
+        it('sends an NFT to the seller / deployer', async () =>
+        {
+            expect(await realEstate.ownerOf(NFT_ID)).to.equal(seller.address)
+        })
+    })
+
+})  
